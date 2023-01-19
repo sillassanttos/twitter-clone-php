959381ace36a60569aa136fcaf9bccffc1632af9
Silas Santos <sillassanttos@outlook.com>
2023-01-19 16:29:32 -0400

    Criando e removendo sessões do usuário logado

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,5 +1,7 @@
 <?php
 
+    session_start();
+
     require_once('db.class.php');
 
     $usuario = $_POST['usuario'];
@@ -20,13 +22,21 @@
 
        if (isset($dados_usuario['usuario'])) {
 
+            $_SESSION['usuario'] = $dados_usuario['usuario'];
+            
+            $_SESSION['email'] = $dados_usuario['email'];
 
+            header('Location: home.php');
 
        } else {
+
             header('Location: index.php?erro=1');
+
        }
 
+
     } else {
+
         echo 'Erro ao efetuar consulta no banco de dados';
     }
 
