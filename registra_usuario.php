3ea29cce17867d43d4abe9b4a4db9bf821713d90
Silas Santos <sillassanttos@outlook.com>
2023-01-19 10:19:18 -0400

    Conectando e inserindo novos usuarios na base de dados

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,7 +1,27 @@
 <?php
 
-    $_POST['usuario'];
-    $_POST['email'];
-    $_POST['senha'];
+    require_once('db.class.php');
+
+    $usuario = $_POST['usuario'];
+
+    $email = $_POST['email'];
+
+    $senha = $_POST['senha'];
+
+    $objDb = new db();
+    
+    $link = $objDb -> conecta_mysql();
+
+    $sql = " insert into usuarios(usuario, email, senha) values('$usuario', '$email', '$senha')";
+
+    if (mysqli_query($link, $sql)) {
+
+        echo 'Usuários registrado com sucesso';
+
+    } else {
+
+        echo 'Erro ao registrar o usuário';
+        
+    }
 
 ?>
