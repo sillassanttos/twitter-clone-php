4dbf14c09dba38c269d887d79e5988eafc642102
Silas Santos <sillassanttos@outlook.com>
2023-01-19 17:29:04 -0400

    Valida usuário e email ja cadastrado

--- registra_usuario.php
+++ registra_usuario.php
@@ -8,9 +8,55 @@
 
     $senha = md5($_POST['senha']);
 
+    $usuario_existe = false;
+
+    $email_existe = false;
+
     $objDb = new db();
     
     $link = $objDb -> conecta_mysql();
+
+    $sql = "select * from usuarios where usuario = '$usuario'";
+
+    if ($resultado_id = mysqli_query($link, $sql)) {
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        $usuario_existe = isset($dados_usuario['usuario']);
+
+    } 
+
+    $sql = "select * from usuarios where email = '$email'";
+
+    if ($resultado_id = mysqli_query($link, $sql)) {
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        $email_existe = isset($dados_usuario['usuario']);
+
+    }  
+    
+    if ($usuario_existe || $email_existe) {
+
+        $retorno_get = '';
+
+        if ($usuario_existe) {
+
+            $retorno_get .= "erro_usuario=1&";
+
+        }
+
+        if ($email_existe) {
+
+            $retorno_get .= "erro_email=1&";
+
+        }
+
+        header('Location: inscrevase.php?' . $retorno_get);
+
+        die();
+
+    }
 
     $sql = " insert into usuarios(usuario, email, senha) values('$usuario', '$email', '$senha')";
 
